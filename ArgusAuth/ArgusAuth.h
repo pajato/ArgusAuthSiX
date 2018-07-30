59772d0ca6b9ba17b8cd56c0fa779f85e9b45905
Paul Michael Reilly <pmr@pajato.com>
2018-07-30 12:22:03 -0400

    Add initial authentication code.
    Rationale:
    =========
    
    This commit adds the initial authentication code as a Core or Domain layer. See the README.md file for more details. The commit introduces the use of Carthage to manage dependencies on third party frameworks.
    
    File changes:
    ============
    
    modified:   .gitignore
    
    - Summary: ignore Carthage build artifacts and checkouts as well as macos .DS_Store files.
    
    new file:   ArgusAuth.xcodeproj/project.pbxproj
    new file:   ArgusAuth.xcodeproj/project.xcworkspace/contents.xcworkspacedata
    new file:   ArgusAuth.xcodeproj/project.xcworkspace/xcshareddata/IDEWorkspaceChecks.plist
    new file:   ArgusAuth/ArgusAuth.h
    new file:   ArgusAuth/ArgusAuth.swift
    new file:   ArgusAuth/Info.plist
    new file:   ArgusAuthTests/ArgusAuthTests.swift
    new file:   ArgusAuthTests/Info.plist
    
    - Summary: Xcode controlled and generated files.
    
    new file:   Cartfile
    new file:   Cartfile.resolved
    
    - Summary: Carthage support files.
    
    modified:   README.md
    
    - Summary: initial documentation.

---
+++ ArgusAuth/ArgusAuth.h
@@ -0,0 +1,19 @@
+//
+//  ArgusAuth.h
+//  ArgusAuth
+//
+//  Created by Paul Reilly on 7/29/18.
+//  Copyright © 2018 pajato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ArgusAuth.
+FOUNDATION_EXPORT double ArgusAuthVersionNumber;
+
+//! Project version string for ArgusAuth.
+FOUNDATION_EXPORT const unsigned char ArgusAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArgusAuth/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ArgusAuthSiX
 Argus authentication module for the Swift-iOS-Xcode environment
+
+## Features
+
+The authentication module provides a validation feature to support Argus login methods, initially email and password
